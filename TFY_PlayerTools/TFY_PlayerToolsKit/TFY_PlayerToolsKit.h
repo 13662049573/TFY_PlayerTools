6af4081c8e99e7c6776fcb8c9d829c21645a164b
田风有 <420144542@qq.com>
2023-03-16 15:08:35 +0800

    修改问题
    修改问题

--- TFY_PlayerTools/TFY_PlayerToolsKit/TFY_PlayerToolsKit.h
+++ TFY_PlayerTools/TFY_PlayerToolsKit/TFY_PlayerToolsKit.h
@@ -4,7 +4,7 @@
 //
 //  Created by 田风有 on 2020/9/9.
 //  Copyright © 2020 田风有. All rights reserved.
-//  最新版本号：2.2.1
+//  最新版本号：2.2.1 -> 2.2.2
 
 #import "TFY_PlayerTool.h"
 #import "TFY_PlayerView.h"
